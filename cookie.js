e054b7b8aa4782d72b80a9ab7b8e46c7e8c37899
lawrghita <laurghita@gmail.com>
2019-07-19 19:09:53 +0000

    Clear logs

--- cookie.js
+++ cookie.js
@@ -3,11 +3,9 @@ function setCookie(cookieName, cookieValue, cookieDayAlive) {
     const sameSite = "strict"
     const today = new Date();
     var dayExpire = new Date();
-    // console.log(today, dayExpire);
     millisecondsOnDay = 24 * 60 * 60 * 1000;
     millisecondsAlive = today.getTime() + cookieDayAlive * millisecondsOnDay;
     dayExpire.setTime(millisecondsAlive);
-    // console.log(dayExpire);
     var timeStamp = dayExpire.toUTCString();
     var cookie = cookieName + "=" + cookieValue + "; " + "expires=" + timeStamp + "; path=" + location + "; SameSite=" + sameSite + ";";
     // console.log(cookie);
@@ -15,35 +13,33 @@ function setCookie(cookieName, cookieValue, cookieDayAlive) {
 }
 
 function valueCookie(cookieName) {
-    // const location = "/";
     var decodedCookie = decodeURIComponent(document.cookie);
-    // console.log(decodedCookie);
+    console.log(decodedCookie);
     var position = decodedCookie.search(cookieName);
+    // console.log("Position:", position);
+    if (position == -1) return (''); // exit to setting the cookie with setCookie()
+
     decodedCookie = decodedCookie.substring(position, position + decodedCookie.length);
     var valueTMP = decodedCookie.substring(cookieName.length + 1);
-    // console.log("TMP:",valueTMP);
     var value = "";
     i = 0;
     while (valueTMP[i] != "" && valueTMP[i] != ";" && i < valueTMP.length) {
         value += valueTMP[i];
         i = i + 1;
     }
-    // console.log(decodedCookie," Check:",cookieName," Value:",value);
+    // console.log(decodedCookie, " Check:", cookieName, " Value:", value);
     return (value);
 }
-setCookie("username", "Laur", 30);
+
+//****************** */
 
 if (valueCookie("username") == "") setCookie("username", "Laur", 30);
 
 ver = valueCookie("indexVersion");
-console.log(ver);
+if (ver === "" || ver === "NaN") ver = 1;
+else ver++;
+setCookie("indexVersion", ver, 30);
 
-if (ver == "" || ver =="NaN") {setCookie("indexVersion", 1, 30);
-document.getElementById("ver").innerHTML = ver;}
-else {
-    ver++;
-    setCookie("indexVersion", ver, 30);
-}
 document.getElementById("ver").innerHTML = ver;
 
 
