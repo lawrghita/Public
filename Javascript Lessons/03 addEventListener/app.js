91700a00fe5abf8f9ebb08a306a475823615f7bc
lawrghita <laurghita@gmail.com>
2019-07-22 18:56:18 +0000

    forEach addEventListener

--- Javascript Lessons/03 addEventListener/app.js
+++ Javascript Lessons/03 addEventListener/app.js
@@ -1,10 +1,13 @@
 button = document.querySelector("button");
 paragraph = document.querySelector("p");
-console.log(button);
-var nbClicked=1;
-button.addEventListener("click",function(){
-paragraph.textContent = button.textContent + " clicked "+nbClicked+" times";
-nbClicked++;
+li = document.querySelectorAll("li");
+console.log(button, paragraph, li);
+
+var nbClicked = 1;
+button.addEventListener("click", function() {
+
+    paragraph.textContent = button.textContent + " clicked " + nbClicked + " times";
+    nbClicked++;
 });
 // var original = true
 // paragraph.addEventListener("mouseover",function(){
@@ -12,9 +15,24 @@ nbClicked++;
 //      else paragraph.style.color="black";
 //      original = !original;
 // });
- paragraph.addEventListener("mouseover",function(){
- paragraph.style.color="red";
- });
-  paragraph.addEventListener("mouseleave",function(){
- paragraph.style.color="black";
- });
+paragraph.addEventListener("mouseover", function() {
+    paragraph.style.color = "red";
+});
+paragraph.addEventListener("mouseleave", function() {
+    paragraph.style.color = "black";
+});
+
+
+li.forEach(function(item) {
+    item.addEventListener("click", function() {
+        if (this.style.color == "blue")
+            this.style.color = "black";
+        else this.style.color = "blue";
+    });
+});
+
+
+
+// addEventListener("click", function() {
+//     this.style.color = "blue";
+// });
