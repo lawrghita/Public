fcbdb1a9d9ad285d97120f346f67070aa5abd510
lawrghita <laurghita@gmail.com>
2019-07-23 21:13:54 +0300

    Code more clear and reusable with mySwitch function

--- Javascript Lessons/03 addEventListener/app.js
+++ Javascript Lessons/03 addEventListener/app.js
@@ -1,49 +1,53 @@
 (function (window, document, undefined) {  /*setting a local scope*/
     "use strict";                         /*no console debug*/
-  
-let button = document.querySelector("button");
-let paragraph = document.querySelector("p");
-let li = document.querySelectorAll("li");
-let h5 = document.querySelector("h5");
 
-console.log(button, paragraph, li,h5);
+    let button = document.querySelector("button");
+    let paragraph = document.querySelector("p");
+    let li = document.querySelectorAll("li");
+    let h5 = document.querySelector("h5");
+
+    console.log(button, paragraph, li, h5);
 
 
-var nbClicked = 1;
-button.addEventListener("click", function () {
+    var nbClicked = 1;
+    button.addEventListener("click", function () {
 
-    paragraph.textContent = button.textContent + " clicked " + nbClicked + " times";
-    nbClicked++;
-});
-// var original = true
-// paragraph.addEventListener("mouseover",function(){
-//     if (original) paragraph.style.color="red";
-//      else paragraph.style.color="black";
-//      original = !original;
-// });
-
-// or this doing the same but more readable code:
-
-paragraph.addEventListener("mouseover", function () {
-    paragraph.style.color = "red";
-});
-paragraph.addEventListener("mouseleave", function () {
-    paragraph.style.color = "black";
-});
-
-// callback with parameters :
-let color1 = "green";
-let color2 = "red";
-li.forEach(function (item) {
-    item.addEventListener("click", function () {
-        if (this.style.color == color1)
-            this.style.color = color2;
-        else this.style.color = color1;
+        paragraph.textContent = button.textContent + " clicked " + nbClicked + " times";
+        nbClicked++;
     });
-}, color1, color2);
+    // var original = true
+    // paragraph.addEventListener("mouseover",function(){
+    //     if (original) paragraph.style.color="red";
+    //      else paragraph.style.color="black";
+    //      original = !original;
+    // });
 
-h5.addEventListener("mouseover", function() {
-    h5.classList.toggle("blue");    
-});
+    // or this doing the same but more readable code:
+
+    paragraph.addEventListener("mouseover", function () {
+        paragraph.style.color = "red";
+    });
+    paragraph.addEventListener("mouseleave", function () {
+        paragraph.style.color = "black";
+    });
+
+    // callback with parameters :
+    // let color1 = "green";
+    // let color2 = "red";
+    li.forEach(function (item) {
+        item.addEventListener("click", function () {
+            mySwitch(item, "green", "blue");
+        });
+    });
+
+    h5.addEventListener("mouseover", function () {
+        h5.classList.toggle("blue");
+    });
+
+    function mySwitch(item, color1, color2) {
+        if (item.style.color == color1)
+            item.style.color = color2;
+        else item.style.color = color1;
+    }
 
 })(window, document);
