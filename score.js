16570d2e8e31e066770aa77b3969b59262843858
lawrghita <laurghita@gmail.com>
2019-07-26 09:18:07 +0000

    index of indexs

--- score.js
+++ score.js
@@ -1,35 +1,6 @@
 "use strict";
-let result = document.getElementById("result");
-let targeth = document.getElementById("targeth");
-let player1Score = document.getElementById("player1");
-let player2Score = document.getElementById("player2");
-let namePlayer1 = document.getElementById("namePlayer1");
-let namePlayer2 = document.getElementById("namePlayer2");
-let reset = document.getElementById("reset");
+location = window.location.href;
+id01 = document.getElementById("01");
 
+id01.href = location;
 
-player1Score.textContent = 0;
-player2Score.textContent = 0;
-player1Score.addEventListener("click", function () {
-    increment(this);
-});
-player2Score.addEventListener("click", function () {
-    increment(this);
-});
-reset.addEventListener("click", function () {
-    location.reload();
-});
-
-function increment(item) {
-    item.textContent = parseInt(item.textContent) + 1;
-    console.log(item.textContent, parseInt(targeth.textContent));
-    if (item.textContent == parseInt(targeth.textContent)) {
-        player1Score.disabled = true;
-        player2Score.disabled = true;
-        item.style.color = "green";
-        let winner = namePlayer2.textContent;
-        if (item.id == "player1") winner = namePlayer1.textContent;
-        result.textContent = "Winner is " + winner;
-        reset.style.display = "inline-block";
-    }
-};
