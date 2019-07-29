d3d81e86a6a9c92990ed8044c2e46fb74a0d3759
lawrghita <laurghita@gmail.com>
2019-07-30 01:23:58 +0300

    Manual API simulation

--- Javascript Lessons/06 MDN events counter/counter.js
+++ Javascript Lessons/06 MDN events counter/counter.js
@@ -1,22 +1,36 @@
 "use strict";
-let url="https://developer.mozilla.org/en-US/docs/Web/Events";
-
+let url = "https://developer.mozilla.org/en-US/docs/Web/Events";
 var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-  console.log(this.responseXML.title);
-  console.log('this',this);
-  console.log("XML",this.responseXML);
-  let x = this.responseXML;
-  console.log("X",x.querySelectorAll('a'));
-  
-  
-  
-//   console.log("ALL",this.responseXML.all);
-//   let a = this.responseXML.all;
-//   console.log('a', a[1,2]);
-  
+let myDiv = document.getElementById('id')
+
+xhr.onload = function () {
+  let xml = this.responseXML;
+  let eventAnchors = xml.querySelectorAll('tr td a')
+  let index = 0;
+  while (index < eventAnchors.length) {
+    let element = eventAnchors[index];
+   
+    if (element.href.includes("/en-US/docs/Web")
+      && !element.href.includes("API/")
+      && !element.href.includes("HTML/")
+      && !element.href.includes("Manifest")
+      && !element.href.includes("CSS/")
+      && !element.href.includes("DOM/")
+      && !element.href.includes("XUL")) {
+      const nameEvent = element.href.slice(element.href.lastIndexOf('/') + 1);
+      noEvent++;
+      console.log(element.href, nameEvent, noEvent);
+      text += "<p>"+noEvent+" <a href='" + element.href + "'>" + nameEvent + "</a>";
+    }
+    index++;
+  }
 }
 xhr.open("GET", url);
 xhr.responseType = "document";
+let noEvent = 0;
+let text = "";
 xhr.send();
-
+setTimeout(function () {
+  console.log("Numbers of events ", noEvent);
+  myDiv.innerHTML = text;
+}, 2000);
