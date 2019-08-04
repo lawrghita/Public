ae393f8b60cba9a6577a98f4d7538675b8208200
lawrghita <laurghita@gmail.com>
2019-08-04 15:29:16 +0300

    Insert RGB Color

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ id05.href = "Javascript%20Lessons/05%20hover%20list/index.html"
 const id06 = document.getElementById("JS06");
 id06.href = "Javascript%20Lessons/06%20MDN%20events%20counter/index.html"
 
+const id07 = document.getElementById("JS07");
+id07.href = "Javascript%20Lessons/07%20RGB%20Color/index.html"
+
+
 // console.log(id01,id02,id03,id04,id05);
 
 
