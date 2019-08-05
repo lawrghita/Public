2a67c3708c7a8a35b89a87a0c86cba5116777a18
lawrghita <laurghita@gmail.com>
2019-08-05 20:32:23 +0300

    Final logic implemented

--- Javascript Lessons/07 RGB Color/rgb.js
+++ Javascript Lessons/07 RGB Color/rgb.js
@@ -1,11 +1,12 @@
 "use strict";
-let cells = document.querySelectorAll('.show');
+let cells = document.querySelectorAll('.show');;
+//  document.querySelectorAll('.show');
 const rgbQuestion = document.getElementById("rgbQuestion");
 const reset = document.getElementById("reset");
+const result = document.getElementById("result");
 const easy = document.getElementById("easy");
 const hard = document.getElementById("hard");
 let hardHide = document.querySelectorAll("#hide td");
-
 initialize();
 
 reset.addEventListener('click', function () {
@@ -19,6 +20,7 @@ easy.addEventListener('click', function () {
     initialize();
 });
 hard.addEventListener('click', function () {
+
     for (let index = 0; index < hardHide.length; index++) {
         hardHide[index].classList = "hide";
     }
@@ -26,28 +28,44 @@ hard.addEventListener('click', function () {
 });
 
 // ***.........
+function correct() {
+    result.textContent = 'CORRECT';
+    cells.forEach(cell => {
+        cell.style.backgroundColor = rgbQuestion.textContent;
+        console.log('set', cell.style.backgroundColor);
+    });
+}
+function error() {
 
+
+}
+function vclick() {
+    const rgbClicked = this.style.backgroundColor.toUpperCase();
+    console.log('clicked', this, rgbClicked, rgbQuestion.textContent);
+    if (rgbClicked === rgbQuestion.textContent) correct();
+    else {
+        result.textContent = 'Try Again';
+        this.style.backgroundColor = 'black';
+    }
+}
 function initialize() {
     cells = document.querySelectorAll('.show');
-    cells.forEach(function (item, index) {
+    result.textContent = '';
+    for (let index = 0; index < cells.length; index++) {
         const randRed = Math.round((Math.random() * 255));
         const randGreen = Math.round((Math.random() * 255));
         const randBlue = Math.round((Math.random() * 255));
-        item.style = "background-color:rgb(" + randRed + ", " + randGreen + ", " + randBlue + ");";
-        item.innerHTML = index + "<p>" + randRed + ", " + randGreen + ", " + randBlue;
-        // console.log('this', item);
-    });
+        cells[index].style = "background-color:rgb(" + randRed + ", " + randGreen + ", " + randBlue + ");";
+        cells[index].innerHTML = index + "<p>" + randRed + ", " + randGreen + ", " + randBlue;
+        cells[index].addEventListener('click', vclick);
+    }
     console.log('cells', cells);
-    // console.log('rgbQ', rgbQuestion);
-    randomRGB();
+    randomRGB(cells);
 }
-
-
-function randomRGB() {
+function randomRGB(cells) {
     const answerCell = Math.floor((Math.random() * cells.length));
-    // console.log(answerCell, ":", cells.length-1);
     const randRed = cells[answerCell].style.backgroundColor;
-    // console.log(":", randRed);
     rgbQuestion.textContent = randRed.toUpperCase();
-
+    console.log("Index for the random color: ", answerCell);
+    return answerCell;
 }
