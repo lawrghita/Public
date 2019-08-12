e0876b0faac8817bad86d5472af46c8e17fd08cd
lawrghita <laurghita@gmail.com>
2019-08-12 22:23:16 +0300

    Finalized RGB Demo

--- Javascript Lessons/07 RGB Color/rgb.js
+++ Javascript Lessons/07 RGB Color/rgb.js
@@ -1,58 +1,80 @@
 'use strict';
-let rows = document.querySelector ('#rows');
-const b = rows.getCom;
-console.log (rows, 'back');
-let cells = document.querySelectorAll ('.show');
+let rows = document.querySelector('#rows');
+let cells = document.querySelectorAll('.show');
+const banner = document.querySelector('#banner');
+const rgbQuestion = document.getElementById('rgbQuestion');
+const reset = document.getElementById('reset');
+const result = document.getElementById('result');
+const easy = document.getElementById('easy');
+const hard = document.getElementById('hard');
+let hardHide = document.querySelectorAll('#hide td');
+const hoverable = document.querySelectorAll('.hover');
 
-const rgbQuestion = document.getElementById ('rgbQuestion');
-const reset = document.getElementById ('reset');
-const result = document.getElementById ('result');
-const easy = document.getElementById ('easy');
-const hard = document.getElementById ('hard');
-let hardHide = document.querySelectorAll ('#hide td');
-initialize ();
+const bannerBackgroundColor = getComputedStyle(banner).backgroundColor;
+initialize();
 
-reset.addEventListener ('click', function () {
-  initialize ();
+reset.addEventListener('click', function () {
+  initialize();
 });
-easy.addEventListener ('click', function () {
+hard.addEventListener('click', function () {
   for (let index = 0; index < hardHide.length; index++) {
     hardHide[index].classList = 'show';
   }
-  initialize ();
+  initialize();
 });
-hard.addEventListener ('click', function () {
+easy.addEventListener('click', function () {
   for (let index = 0; index < hardHide.length; index++) {
     hardHide[index].classList = 'hide';
   }
-  initialize ();
+  initialize();
+});
+
+hoverable.forEach(element => {
+  const lineTextColor = getComputedStyle(element).color;
+  element.addEventListener('click', function () {
+    element.style.backgroundColor = getComputedStyle(banner).backgroundColor;
+ });
+  element.addEventListener('mouseover', function () {
+     element.style.backgroundColor = getComputedStyle(banner).backgroundColor;
+     element.style.color = 'white';
+  });
+  element.addEventListener('mouseout', function () {
+    element.style.backgroundColor = "white";
+    element.style.color = lineTextColor;
+  });
 });
 
 // ***.........
-function correct () {
-    //** on correct guess, display CORRECT on middle and make all squares that color */
+function correct() {
+  'use strict';
+  //** on correct guess, display CORRECT on middle and make all squares that color */
   result.textContent = 'CORRECT';
-  cells.forEach (cell => {
+  cells.forEach(cell => {
     cell.style.backgroundColor = rgbQuestion.textContent;
   });
+  banner.style.backgroundColor = rgbQuestion.textContent;
 }
 
-function actionClick () {
-  const rgbClicked = this.style.backgroundColor.toUpperCase ();
-  if (rgbClicked === rgbQuestion.textContent) correct ();
+function actionClick() {
+  'use strict';
+  const rgbClicked = this.style.backgroundColor.toUpperCase();
+  if (rgbClicked === rgbQuestion.textContent) correct();
   else {
     result.textContent = 'Try Again';
-    this.style.backgroundColor = getComputedStyle (rows).backgroundColor;
+     //** chose color on air */
+    this.style.backgroundColor = getComputedStyle(rows).backgroundColor;
   }
 }
-function initialize () {
+function initialize() {
+  'use strict';
   //** restart all colors */
-  cells = document.querySelectorAll ('.show');
+  banner.style.backgroundColor = bannerBackgroundColor;
+  cells = document.querySelectorAll('.show');
   result.textContent = '';
   for (let index = 0; index < cells.length; index++) {
-    const randRed = Math.round (Math.random () * 255);
-    const randGreen = Math.round (Math.random () * 255);
-    const randBlue = Math.round (Math.random () * 255);
+    const randRed = Math.round(Math.random() * 255);
+    const randGreen = Math.round(Math.random() * 255);
+    const randBlue = Math.round(Math.random() * 255);
     cells[index].style =
       'background-color:rgb(' +
       randRed +
@@ -61,14 +83,14 @@ function initialize () {
       ', ' +
       randBlue +
       ');';
-    cells[index].addEventListener ('click', actionClick);
+    cells[index].addEventListener('click', actionClick);
   }
-  console.log ('cells', cells);
-  choseRandomOneSquareRGB (cells);
+  choseRandomOneSquareRGB(cells);
 }
-function choseRandomOneSquareRGB (cells) {
-  const answerCell = Math.floor (Math.random () * cells.length);
+function choseRandomOneSquareRGB(cells) {
+  'use strict';
+  const answerCell = Math.floor(Math.random() * cells.length);
   const randRed = cells[answerCell].style.backgroundColor;
-  rgbQuestion.textContent = randRed.toUpperCase ();
+  rgbQuestion.textContent = randRed.toUpperCase();
   return answerCell;
 }
