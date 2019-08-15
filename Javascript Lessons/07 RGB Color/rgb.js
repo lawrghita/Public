8e38d0782e68c22613003c97cfd169622781e202
lawrghita <laurghita@gmail.com>
2019-08-15 13:20:48 +0300

    Keeping the styles inside css, switching using diferent class, not javascript

--- Javascript Lessons/07 RGB Color/rgb.js
+++ Javascript Lessons/07 RGB Color/rgb.js
@@ -1,4 +1,7 @@
 'use strict';
+const correctGuessMessage = "CORRECT";
+const choseText = "CHOSE:"
+const newColors = "REFRESH ?"
 let rows = document.querySelector('#rows');
 let cells = document.querySelectorAll('.show');
 const banner = document.querySelector('#banner');
@@ -8,47 +11,57 @@ const result = document.getElementById('result');
 const easy = document.getElementById('easy');
 const hard = document.getElementById('hard');
 let hardHide = document.querySelectorAll('#hide td');
-const hoverable = document.querySelectorAll('.hover');
-
 const bannerBackgroundColor = getComputedStyle(banner).backgroundColor;
+
 initialize();
 
-reset.addEventListener('click', function() {
-    initialize();
-});
-hard.addEventListener('click', function() {
-    for (let index = 0; index < hardHide.length; index++) {
-        hardHide[index].classList = 'show';
+reset.addEventListener('click', function () {
+    if (result.textContent == correctGuessMessage) {
+        //** Inactivate the reset till the correct color is chosen  */
+        initialize();
     }
-    initialize();
+
 });
-easy.addEventListener('click', function() {
+reset.addEventListener('mouseover', function () {
+    if (result.textContent == correctGuessMessage) {
+        this.classList.remove("normalLineElement");
+        this.classList.add("gameType");
+    }
+});
+reset.addEventListener('mouseout', function () {
+    this.classList.add("normalLineElement");
+    this.classList.remove("gameType");
+});
+
+
+easy.addEventListener('click', function () {
     for (let index = 0; index < hardHide.length; index++) {
         hardHide[index].classList = 'hide';
     }
+    this.classList.add("gameType");
+    this.classList.remove("normalLineElement");
+    hard.classList.remove("gameType");
+    hard.classList.add("normalLineElement");
     initialize();
 });
 
-hoverable.forEach(element => {
-    const lineTextColor = getComputedStyle(element).color;
-    element.addEventListener('click', function() {
-        element.style.backgroundColor = getComputedStyle(banner).backgroundColor;
-    });
-    element.addEventListener('mouseover', function() {
-        element.style.backgroundColor = getComputedStyle(banner).backgroundColor;
-        element.style.color = 'white';
-    });
-    element.addEventListener('mouseout', function() {
-        element.style.backgroundColor = "white";
-        element.style.color = lineTextColor;
-    });
+hard.addEventListener('click', function () {
+    for (let index = 0; index < hardHide.length; index++) {
+        hardHide[index].classList = 'show';
+    }
+    this.classList.add("gameType");
+    this.classList.remove("normalLineElement");
+    easy.classList.remove("gameType");
+    easy.classList.add("normalLineElement")
+    initialize();
 });
 
 // ***.........
 function correct() {
     'use strict';
     //** on correct guess, display CORRECT on middle and make all squares that color */
-    result.textContent = 'CORRECT';
+    result.textContent = correctGuessMessage;
+    reset.textContent = newColors;
     cells.forEach(cell => {
         cell.style.backgroundColor = rgbQuestion.textContent;
     });
@@ -60,7 +73,6 @@ function actionClick() {
     const rgbClicked = this.style.backgroundColor.toUpperCase();
     if (rgbClicked === rgbQuestion.textContent) correct();
     else {
-        result.textContent = 'Try Again';
         //** chose color on air */
         this.style.backgroundColor = getComputedStyle(rows).backgroundColor;
     }
@@ -71,6 +83,7 @@ function initialize() {
     banner.style.backgroundColor = bannerBackgroundColor;
     cells = document.querySelectorAll('.show');
     result.textContent = '';
+    reset.textContent = choseText;
     for (let index = 0; index < cells.length; index++) {
         const randRed = Math.round(Math.random() * 255);
         const randGreen = Math.round(Math.random() * 255);
