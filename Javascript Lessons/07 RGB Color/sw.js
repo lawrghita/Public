32f10cd94a29bfa064be969e5f6bdfc590371013
lawrghita <laurghita@gmail.com>
2019-08-17 01:37:45 +0300

    Cache Storage disapear ?

--- Javascript Lessons/07 RGB Color/sw.js
+++ Javascript Lessons/07 RGB Color/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var cacheName = 'law-cache-v2.1';
+var cacheName = 'law-cache-v2.3.3';
 self.addEventListener('install', (event) => {
    console.log('Install check',event)
     event.waitUntil(
@@ -15,6 +15,9 @@ self.addEventListener('install', (event) => {
           'rgb.css'
          ]);
       })
+      .then(function(){
+        console.log('Worker: install Completed');
+      })
     );
   });
 
@@ -22,14 +25,26 @@ self.addEventListener('install', (event) => {
     event.waitUntil(self.clients.claim());
   });
 
-  self.addEventListener('push', event => {
-    console.log(event.target);
-    event.waitUntil((event) => {
-      console.log(event.request);
-    });
-  });
+  // self.addEventListener('push', event => {
+  //   console.log(event.target);
+  //   event.waitUntil((event) => {
+  //     console.log(event.request);
+  //   });
+  // });
  
-  self.addEventListener('fetch', event => {
+  self.addEventListener('fetch', function(event) {
+    console.log('WORKER: fetch event in progress.3', self.registration.scope);
+    if (event.request.method !== 'GET') {
+      /* If we don't block the event as shown below, then the request will go to
+         the network as usual.
+      */
+      console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+      return;
+    }
+    /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
+       Fulfillment result will be used as the response, and rejection will end in a
+       HTTP response indicating failure.
+    */
     event.respondWith(
       caches.open(cacheName)
         .then(cache => cache.match(event.request, {ignoreSearch: true}))
