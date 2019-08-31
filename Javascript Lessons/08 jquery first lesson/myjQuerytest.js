793f04a8de090965f4eb41b0acf2dc4669a5ea2a
lawrghita <laurghita@gmail.com>
2019-08-31 23:57:57 +0300

    some plays

---
+++ Javascript Lessons/08 jquery first lesson/myjQuerytest.js
@@ -0,0 +1,21 @@
+"use strict";
+var purpleBackground={
+    background: "purple"
+}
+var wide200={
+    width: "200px"
+}
+var orangeBorder={
+    border: "1px solid orange"
+}
+var fontPink={
+    color: "pink"
+}
+
+
+console.log($("div"));
+
+$("div").css(purpleBackground);
+$(".highlight").css(wide200);
+$("#third").css(orangeBorder).css(fontPink);
+$("div:first-of-type").css(fontPink);
