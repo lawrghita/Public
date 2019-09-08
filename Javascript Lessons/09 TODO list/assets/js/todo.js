353c1e528edbcebd470df8bfe1e83a07252662d1
lawrghita <laurghita@gmail.com>
2019-09-08 21:42:59 +0300

    short li

--- Javascript Lessons/09 TODO list/assets/js/todo.js
+++ Javascript Lessons/09 TODO list/assets/js/todo.js
@@ -1,4 +1,29 @@
 let version = document.querySelector("#version");
-const time=document.lastModified
-version.textContent="v.0.3 "+time;
-console.log(time,$("body").text());
+const time = document.lastModified;
+version.textContent = "v.0.5 ";
+console.log(time);
+
+// const colorClicked = "rgb(128, 128, 0)"; //set up color for TODOs finalized - gray?
+// const liClicked = { color: colorClicked, "text-decoration": "line-through" };
+// const liNormal = { color: "black", "text-decoration": "none" };
+// $("li").click(function() {
+//   // console.log("1:", $(this), $(this).css("color"), $(this).css("font"));
+//   if ($(this).css("color") === colorClicked) {
+//     // toggle between liNormal/liClicked
+//     $(this).css(liNormal);
+//   } else {
+//     $(this).css(liClicked);
+//   }
+//   console.log(
+//     "2:",
+//     $(this),
+//     $(this).css("color"),
+//     $(this).css("text-decoration")
+//   );
+// });
+
+// or
+
+$("li").click(function() {
+$(this).toggleClass("completed");
+});
