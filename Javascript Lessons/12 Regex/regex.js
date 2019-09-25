09e022b3177575983359af520d12335b51009b37
lawrghita <laurghita@gmail.com>
2019-09-25 23:09:37 +0300

    Regex 2th day

--- Javascript Lessons/12 Regex/regex.js
+++ Javascript Lessons/12 Regex/regex.js
@@ -1,3 +1,14 @@
+"use strict";
+myRegex(/ese|\s|ach|final/gi, "testanytyoeofspaces newlinese   TABis3SPACEScgnMetach\\n\nSecondLine\SinAnegationfinal");
+myRegex(/test|\n|Sec/gi, "test new line secgnMetach\\n\nSecond Line sdsd");
+myRegex(/(Met)|\t|final/g, "testMetacharacterTAB(\t)(\x09)final There are no horizontal(        )tabulation entities in HTML");
+myRegex(/\D/g, "Metacharacter d represent not 0-9c = not digits");
+myRegex(/\d/g, "Metacharacter d represent0-9c= digits");
+myRegex(/\W/g, "Metacharacter W represent negation of  a-zA-z0-9_ = letters_numerals & underscore so #%$#^#@#$");
+myRegex(/\w/g, "Metacharacter w represent a-zA-z0-9_ = letters_numerals & underscore so_not #%$#^#@#$");
+myRegex(/./g, "Metacharacter . Point represent anything ");
+myRegex(/cat|dog|or/gi, "Alternation or OR operator This cat is dog Thedog  test cat");
+myRegex(/[ -~]/g, "chose All ASCI range from space ' ' to tilda '~'  %$543!@.,'';=<>{}\= table:");
 myRegex(/[a-z]/g, "mach just vowels  pair from 222 because last pair have the middle 2 consumed");
 myRegex(/[aeiouy]/g, "mach just vowels  pair from 222 because last pair have the middle 2 consumed");
 myRegex(/22/, "mach just first pair from 222 because last pair have the middle 2 consumed");
@@ -7,7 +18,7 @@ myRegex(/the/, "This is The thext test");
 myRegex(/th/, "This is Theth test");
 myRegex(/(?<=<).*?(?=>)/g, " Return whats betwen <brackets> thet <ea>regexp");
 
-v = new RegExp(/22/, 'g');
+// v = new RegExp(/22/, 'g');
 // console.log(v, v.exec("22 22"), "22 22".match(v));
 //
 // var vcode = document.querySelectorAll(".code");
@@ -26,9 +37,10 @@ function myRegex(vFcode, vFinput) {
     document.body.appendChild(newDiv);
     newDiv = document.createElement("div");
     newDiv.className = "result";
+    let result;
     result = vFinput.match(vFcode);
     newDiv.textContent = result;
-    console.log(result, result.index, result.length, result[0]);
+    // console.log(result, result.index, result.length, result[0]);
     document.body.appendChild(newDiv);
     document.body.appendChild(document.createElement("br"));
 
