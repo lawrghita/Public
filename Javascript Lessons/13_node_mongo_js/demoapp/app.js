8185d649570af4125d72f5898e0b14f7de9a4138
lawrghita <laurghita@gmail.com>
2019-11-05 01:22:52 +0200

    facebook node 2FA authentification example

---
+++ Javascript Lessons/13_node_mongo_js/demoapp/app.js
@@ -0,0 +1,6 @@
+var catMeLaw = require('cat-me');
+var knockLaw = require('knock-knock-jokes');
+console.log('From App.js','\n\n');
+console.log(catMeLaw());
+console.log(catMeLaw('nyan'));
+console.logknockLaw();
