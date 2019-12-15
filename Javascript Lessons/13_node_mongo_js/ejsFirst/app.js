ceb9b457a52e58392bcd689b373897772a41e66b
lawrghita <laurghita@gmail.com>
2019-12-15 19:21:27 +0200

    ejs firs close

--- Javascript Lessons/13_node_mongo_js/ejsFirst/app.js
+++ Javascript Lessons/13_node_mongo_js/ejsFirst/app.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const style = '/fall/:objects';
 app.use(express.static('public'));  //define location for public static files
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send(`path in GET ${req.path} request`);
+    res.send(`path in GET ${req.path} request must be ${style}`);
 });
 
-app.get('/fall/:objects',function (req,res) {
+app.get(style,function (req,res) {
     var variable=req.params.objects;
     var vpath = req.path.toString();
-    console.log(`${variable}  `, vpath);
-    console.log(`fall obiect ${variable}`);
+  //  console.log(`${variable}  `, vpath);
+  //  console.log(`fall obiect ${variable}`);
    // res.send(`fall obiect ${object}`);
     res.render("fall.ejs",{variable, path: vpath}) //{object} with variables as parameters to .ejs render file}
 });
 
 app.get('*',function (req, res) {
-    res.render("home.ejs");
+    res.render("home.ejs",{variableToUseinEJSfile: style});
   //  res.send(`request path in GET: <p>   ${req.path}  </p> not defined in app`);
 });
 app.listen(port, function () {
