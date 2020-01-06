f009f3e913f078f075ad95a1a661a2fe00dd6603
lawrghita <laurghita@gmail.com>
2020-01-06 22:20:03 +0200

    include partials

--- Javascript Lessons/13_node_mongo_js/ejsLessons/ejsControlFlow/app.js
+++ Javascript Lessons/13_node_mongo_js/ejsLessons/ejsControlFlow/app.js
@@ -33,13 +33,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  var vpath = req.path.toString();
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{path: vpath});
 });
 
 module.exports = app;
