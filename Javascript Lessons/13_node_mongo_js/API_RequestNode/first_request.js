747f9b59ce31ce62e13b2b3bb6778d0ec60b1b97
lawrghita <laurghita@gmail.com>
2020-01-17 23:34:47 +0200

    fr

--- Javascript Lessons/13_node_mongo_js/API_RequestNode/first_request.js
+++ Javascript Lessons/13_node_mongo_js/API_RequestNode/first_request.js
@@ -1,5 +1,5 @@
 var request = require('request');
-request('http://www.laurghita.eu', function (error, response, body) {
+request('http://www.laurghita.eu', function (error, response, body) {   //callback function.... asyncron
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     if(!error && response.statusCode == 200){
