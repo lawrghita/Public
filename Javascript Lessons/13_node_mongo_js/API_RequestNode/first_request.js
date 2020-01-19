1e5ef490dced92207d4a5f24fb9300b3b180819c
lawrghita <laurghita@gmail.com>
2020-01-19 22:57:26 +0200

    npm start script

--- Javascript Lessons/13_node_mongo_js/API_RequestNode/first_request.js
+++ Javascript Lessons/13_node_mongo_js/API_RequestNode/first_request.js
@@ -1,8 +1,16 @@
 var request = require('request');
-request('http://www.laurghita.eu', function (error, response, body) {   //callback function.... asyncron
-    console.log('error:', error); // Print the error if one occurred
+require('locus');
+request('https://jsonplaceholder.typicode.com/users/1', function (error, response, body) {   //callback function.... asyncron
+    if (error) {
+        console.log('Something is Wrong error: ', error); // Print the error if one occurred
+    }
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    if(!error && response.statusCode == 200){
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    if (!error && response.statusCode == 200) {
+        var parsedBody=JSON.parse(body);  //because body is returned as a string
+        console.log(parsedBody);
+      //  eval(locus);
+        console.log(parsedBody["address"]["geo"]["lat"]);
+        console.log(parsedBody.address.geo.lat);
+        console.log(`Latitude ${parsedBody.address["geo"].lat} for the ${parsedBody.name}`);
     }
 });
--- Javascript Lessons/13_node_mongo_js/API_RequestNode/readme.md
+++ Javascript Lessons/13_node_mongo_js/API_RequestNode/readme.md
@@ -6,3 +6,22 @@ from:
 ```
 https://www.npmjs.com/package/request
 ```
+
+
+works once:
+```javascript
+npm install locus --save-dev
+require('locus');
+...
+eval(locus);
+...
+```
+
+put this in package.json
+```
+  "scripts": {
+    "start": "nodemon first_request.js"
+  },
+```
+and then you can run with ```npm start```
+
