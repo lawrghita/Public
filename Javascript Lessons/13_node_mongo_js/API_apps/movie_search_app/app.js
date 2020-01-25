81938a96036e1cff4c207a250ff665f62dda62bb
lawrghita <laurghita@gmail.com>
2020-01-25 22:53:59 +0200

    Movie poster search base working

--- Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/app.js
+++ Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/app.js
@@ -1,22 +1,34 @@
 var request = require("request");
 const express = require('express');
 const app = express();
-const vURI = "http://www.omdbapi.com/?s=california&apikey=thewdb";
+//const vURI = "http://www.omdbapi.com/?s=california&apikey=thewdb";
 app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
+app.get("/",function callBackSearch(req, res) {
+    res.render("search.ejs");
+});
+
+app.get('/results', function callBackPosters(req, res) {
+    vsearchthis=req.query.searchthis;
+    vURI="http://www.omdbapi.com/?s="+vsearchthis+"&apikey=thewdb"
+    console.log(vURI);
     request(vURI, function callback(error, response, body) {
-        res.render("results");
         const parsedData = JSON.parse(body);
-        const poster= `<img src="${parsedData.Search[1].Poster}" alt="talian s Trulli">`;
-        if(!error && response.statusCode == 200){
-            console.log("Title call type 1:",parsedData.Search[2].Title);
-            console.log("Title call type 2:",parsedData["Search"][2]["Title"]);
-            console.log(poster);
+        console.log('!error',!error);
+        console.log('response.statusCode == 200',(response.statusCode == 200));
+        console.log('parsedData["Response"]',parsedData["Response"] == "True");
+
+      //  const poster= `<img src="${parsedData.Search[1].Poster}" alt="talian s Trulli">`;
+        if(!error && response.statusCode == 200 && (parsedData["Response"] == "True")){
+           // console.log("Title call type 1:",parsedData.Search[2].Title);
+           // console.log("Title call type 2:",parsedData["Search"][2]["Title"]);
+           // console.log(poster);
             //res.send(poster);
-            res.render("results",{data: parsedData});
+            console.log((!error && response.statusCode == 200 && (parsedData["Response"] == "True")));
+            res.render("results.ejs",{data: parsedData});
         }
         else {
+            parsedData["Error"]+=" "+vsearchthis;
             res.send(parsedData);
         }
     });
