6a91d7d133146a1290cab76adb8bcc526029b489
lawrghita <laurghita@gmail.com>
2020-01-25 16:25:54 +0200

    movie app

--- Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/app.js
+++ Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/app.js
@@ -1,6 +1,34 @@
-var express = require("express");
-var app = express();
-var rewuest = require("request");
-app.listen(function applisten() {
-    console.log("server is listening");
+var request = require("request");
+const express = require('express');
+const app = express();
+const vURI = "http://www.omdbapi.com/?s=california&apikey=thewdb";
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+    request(vURI, function callback(error, response, body) {
+        res.render("results");
+        const parsedData = JSON.parse(body);
+        const poster= `<img src="${parsedData.Search[1].Poster}" alt="talian s Trulli">`;
+        if(!error && response.statusCode == 200){
+            console.log("Title call type 1:",parsedData.Search[2].Title);
+            console.log("Title call type 2:",parsedData["Search"][2]["Title"]);
+            console.log(poster);
+            //res.send(poster);
+            res.render("results",{data: parsedData});
+        }
+        else {
+            res.send(parsedData);
+        }
+    });
 });
+
+
+
+app.listen(3000, function listenconfirmation(req,result) {
+    console.log('Listen localhost:3000');
+});
+
+
+
+
+
--- Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/readme.md
+++ Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/readme.md
@@ -1,15 +1,15 @@
-
+This modules is installed via npm, --save for info in package-lock.json:
+```
+npm install --save request
+npm install --save ejs
+npm install --save express
 
 put this in package.json
 ```
   "scripts": {
-    "start": "nodemon first_request.js"
+    "start": "nodemon app.js"
   },
 ```
 and then you can run with ```npm start```
 
-This modules is installed via npm, --save for info in package-lock.json:
-```
-npm install --save request
-npm install --save request-promise
 ```
