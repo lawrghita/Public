bac57696ace40e1d766dafdc6aeec20b2c57a154
lawrghita <laurghita@gmail.com>
2020-01-27 19:27:27 +0200

    final ejs

--- Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/app.js
+++ Javascript Lessons/13_node_mongo_js/API_apps/movie_search_app/app.js
@@ -1,9 +1,7 @@
 var request = require("request");
 const express = require('express');
 const app = express();
-//const vURI = "http://www.omdbapi.com/?s=california&apikey=thewdb";
 app.set("view engine", "ejs");
-
 app.get("/",function callBackSearch(req, res) {
     res.render("search.ejs");
 });
@@ -17,13 +15,7 @@ app.get('/results', function callBackPosters(req, res) {
         console.log('!error',!error);
         console.log('response.statusCode == 200',(response.statusCode == 200));
         console.log('parsedData["Response"]',parsedData["Response"] == "True");
-
-      //  const poster= `<img src="${parsedData.Search[1].Poster}" alt="talian s Trulli">`;
         if(!error && response.statusCode == 200 && (parsedData["Response"] == "True")){
-           // console.log("Title call type 1:",parsedData.Search[2].Title);
-           // console.log("Title call type 2:",parsedData["Search"][2]["Title"]);
-           // console.log(poster);
-            //res.send(poster);
             console.log((!error && response.statusCode == 200 && (parsedData["Response"] == "True")));
             res.render("results.ejs",{data: parsedData});
         }
@@ -33,8 +25,6 @@ app.get('/results', function callBackPosters(req, res) {
         }
     });
 });
-
-
 
 app.listen(3000, function listenconfirmation(req,result) {
     console.log('Listen localhost:3000');
