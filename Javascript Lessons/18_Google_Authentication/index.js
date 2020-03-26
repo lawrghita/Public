b4da1da2e96fa6857cf99dc26b41361700632518
lawrghita <laurghita@gmail.com>
2020-03-26 15:19:42 +0200

    Authentication google final

--- Javascript Lessons/18_Google_Authentication/index.js
+++ Javascript Lessons/18_Google_Authentication/index.js
@@ -6,10 +6,6 @@ const app = express();
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
-//  Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,11 +39,7 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
+
 app.get(
   "/",
   passport.authenticate("google", {
@@ -55,11 +47,6 @@ app.get(
   })
 );
 
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 app.get("/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/err" }),
   function(req, res) {
--- Javascript Lessons/18_Google_Authentication/readme.md
+++ Javascript Lessons/18_Google_Authentication/readme.md
@@ -4,11 +4,12 @@ npm i express
 npm i dotenv
 ```
 in .env put the information from google_key.JSON developer personal account
-using passport http://www.passportjs.org/docs/google/
+using passport http://www.passportjs.org/docs/google/ 
+
 ```
 npm install passport
 npm install passport-google-oauth
 ```
 
-User is a personal object must be defined
-.gitignore
+
+.gitignore must contain .env with personal secret hidden
