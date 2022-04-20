749f52167e2b8fffb11fa453a8cb8d634e79acea
laurentiu69 <laurghita@yahoo.com>
2022-04-20 21:15:28 +0300

    Eliminate gallery first index

--- Javascript Lessons/17_Heroku_Mongo_Gallery/app.js
+++ Javascript Lessons/17_Heroku_Mongo_Gallery/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/gallery');
 var galleryRouter = require('./routes/gallery');
 var usersRouter = require('./routes/users');
 
