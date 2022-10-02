36cbce24d5f6c39121eaf100c08d32e83b93e259
Ghita Laurentiu <laurghita@yahoo.com>
2022-10-02 15:25:56 +0300

    Index paths modified without space

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 "use strict";
 const id01 = document.getElementById("JS01");
-id01.href = "Javascript%20Lessons/01%20first_Cookie/index.html";
+id01.href = "Javascript_Lessons/01%20first_Cookie/index.html";
 
 const id02 = document.getElementById("JS02");
-id02.href = "Javascript%20Lessons/02%20set_getAttribute/index.html";
+id02.href = "Javascript_Lessons/02%20set_getAttribute/index.html";
 
 const id03 = document.getElementById("JS03");
-id03.href = "Javascript%20Lessons/03%20addEventListener/index.html";
+id03.href = "Javascript_Lessons/03%20addEventListener/index.html";
 
 const id04 = document.getElementById("JS04");
-id04.href = "Javascript%20Lessons/04%20Score%20Demo/index.html";
+id04.href = "Javascript_Lessons/04%20Score%20Demo/index.html";
 
 const id05 = document.getElementById("JS05");
-id05.href = "Javascript%20Lessons/05%20hover%20list/index.html";
+id05.href = "Javascript_Lessons/05%20hover%20list/index.html";
 
 const id06 = document.getElementById("JS06");
-id06.href = "Javascript%20Lessons/06%20MDN%20events%20counter/index.html";
+id06.href = "Javascript_Lessons/06%20MDN%20events%20counter/index.html";
 
 const id07 = document.getElementById("JS07");
-id07.href = "Javascript%20Lessons/07%20RGB%20Color/index.html";
+id07.href = "Javascript_Lessons/07%20RGB%20Color/index.html";
 
 const id08 = document.getElementById("JS08");
-id08.href = "Javascript%20Lessons/08%20jquery%20first%20lesson/index.html";
+id08.href = "Javascript_Lessons/08%20jquery%20first%20lesson/index.html";
 
 const id09 = document.getElementById("JS09");
-id09.href = "Javascript%20Lessons/09%20TODO%20list/index.html";
+id09.href = "Javascript_Lessons/09%20TODO%20list/index.html";
 
 const id10 = document.getElementById("JS10");
-id10.href = "Javascript%20Lessons/10%20Sound%20Demo/index.html";
+id10.href = "Javascript_Lessons/10%20Sound%20Demo/index.html";
 
 const id11 = document.getElementById("JS11");
-id11.href = "Javascript%20Lessons/11%20TODO%20Workbox/redirect.html";
+id11.href = "Javascript_Lessons/11%20TODO%20Workbox/redirect.html";
 
 const id12 = document.getElementById("JS12");
-id12.href = "Javascript%20Lessons/12%20Regex/index.html";
+id12.href = "Javascript_Lessons/12%20Regex/index.html";
 
 const id17 = document.getElementById("JS17");
 id17.href = "https://boiling-reaches-35811.herokuapp.com/";
